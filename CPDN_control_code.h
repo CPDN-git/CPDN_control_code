05ef3c80b38ffbe4e38cb192b91f7c389925a088
Andy Bowery <abowery@users.noreply.github.com>
2023-09-27 16:47:56 +0100

    Added prototypes for new functions

--- CPDN_control_code.h
+++ CPDN_control_code.h
@@ -51,6 +51,8 @@ bool oifs_get_stat(std::ifstream&, std::string&);
 bool oifs_valid_step(std::string&,int);
 int  print_last_lines(std::string filename, int nlines);
 void update_progress_file(std::string, int, int, std::string, int, int);
+bool read_rcf_file(std::ifstream&, std::string&, std::string&);
+bool read_delimited_line(std::string&, std::string, std::string, int, std::string&);
 
 using namespace std;
 using namespace std::chrono;
