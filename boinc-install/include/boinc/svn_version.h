9043d21e16adec29465914be31834e428dd02258
Andy Bowery <abowery@users.noreply.github.com>
2023-12-08 15:30:11 +0000

    Adding boinc-install files for automated building of the CPDN control code

---
+++ boinc-install/include/boinc/svn_version.h
@@ -0,0 +1,9 @@
+#ifndef SVN_VERSION_H
+#define SVN_VERSION_H
+
+#define SVN_VERSION "2a37aa3ec6f4bb6c6d8eb3fec354d158b76eab68 [https://github.com/BOINC/boinc.git] (galene:/home/abowery/Desktop/OpenIFS/boinc [master]) [Server-Release: server_release/1.x/1.x.0-134-g2a37aa3ec6f4bb6c6d8eb3fec354d158b76eab68]"
+#define GIT_REVISION 0x2a37aa3e
+#define GIT_DATE 1668452707
+#define REPOSITORY_GITHUB_COM_BOINC_BOINC 1
+
+#endif
