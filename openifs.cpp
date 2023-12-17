eae424c13f94582921594a754fd2b81c652e87bf
Andy Bowery <abowery@users.noreply.github.com>
2023-12-17 15:33:27 +0000

    Updated date at top of file

--- openifs.cpp
+++ openifs.cpp
@@ -1,7 +1,7 @@
 //
 // Control code for the OpenIFS application in the climateprediction.net project
 //
-// Written by Andy Bowery (Oxford eResearch Centre, Oxford University) November 2022
+// Written by Andy Bowery (Oxford eResearch Centre, Oxford University) December 2023
 // Contributions from Glenn Carver (ex-ECMWF), 2022->
 //
 
