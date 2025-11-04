ad7d8803a81a5fcb7b2b2e3b7d5c80822e339b31
Glenn Carver <gdcarver@gmail.com>
2025-11-04 20:55:50 +0000

    start of improvements to model test

--- oifs_test_setup.py
+++ oifs_test_setup.py
@@ -1,40 +1,39 @@
 #! /usr/bin/python3
 
 # Script to setup the file structure required to run a test of new checked in code using GitHub Action 
+# or run locally.  This test mimics a workunit for the OpenIFS 43r3 model.
 
-# This script has been written by Andy Bowery (Oxford University, 2024)
-# Modifications by Glenn Carver (CPDN)
+#   Glenn Carver, CPDN Development Team, Nov 2025.
+#   Based on earlier version by Andy Bowery.
 
 if __name__ == "__main__":
 
       import os, secrets, zipfile, shutil
 
-      # Create projects directory
+      # Make sure we're in the 'test' directory, if not then exit with error
+      current_path = os.getcwd()
+      if not current_path.endswith("test"):
+        print("Error: test_setup.py must be run from within the 'test' directory")
+        exit(1)
+
       if not os.path.isdir("projects"):
         os.mkdir("projects")
 
-      # Create slots directory
-      if not os.path.isdir("slots"):
-        os.mkdir("slots")
+      if not os.path.isdir("slot"):
+        os.mkdir("slot")
 
-      script_path = os.getcwd()
-
-      # Change the working path to projects
       os.chdir("projects")
 
-
-      # Produce oifs_43r3_app file
+      # Produce fake oifs_43r3_app file
       with open('oifs_43r3_app_1.00_x86_64-pc-linux-gnu', 'a') as oifs_app_file:
         oifs_app_file.write(secrets.token_hex(4000) + '\n')
       zip_file = zipfile.ZipFile('./oifs_43r3_app_1.00_x86_64-pc-linux-gnu.zip','w')
       zip_file.write('oifs_43r3_app_1.00_x86_64-pc-linux-gnu')
       zip_file.close()
 
+      os.chdir("../slot")
 
-      # Change the working path to slots
-      os.chdir("../slots")
-
-      # Create the init_data.xml, this file is needed by BOINC
+      # Create the init_data.xml, needed by BOINC
       init_data_string="   <app_init_data>\n" +\
                        "     <major_version>0</major_version>\n" +\
                        "     <minor_version>0</minor_version>\n" +\
@@ -43,8 +42,8 @@ if __name__ == "__main__":
                        "     <hostid>0</hostid>\n" +\
                        "     <app_name>openifs</app_name>\n" +\
                        "     <project_preferences></project_preferences>\n" +\
-                       "     <project_dir>"+script_path+"/projects</project_dir>\n" +\
-                       "     <boinc_dir>"+script_path+"</boinc_dir>\n" +\
+                       "     <project_dir>"+current_path+"/projects</project_dir>\n" +\
+                       "     <boinc_dir>"+current_path+"</boinc_dir>\n" +\
                        "     <wu_name>oifs_43r3_NNNN_yyyymmddhh_1_d000_0</wu_name>\n" +\
                        "     <shm_key>0</shm_key>\n" +\
                        "     <slot>0</slot>\n" +\
@@ -73,18 +72,17 @@ if __name__ == "__main__":
       init_data_file.close()
 
 
-      # Create fort.4 file
+      # Create the fake model namelist file, fort.4.
       fort_file_string = "!WU_TEMPLATE_VERSION=43r3-seasonal-20250801\n"+\
                          "!EXPTID=NNNN\n"+\
                          "!UNIQUE_MEMBER_ID=1353\n"+\
                          "!IFSDATA_FILE=ifsdata_0\n" +\
                          "!IC_ANCIL_FILE=ic_ancil_0\n" +\
                          "!CLIMATE_DATA_FILE=clim_data_0\n" +\
-                         "!HORIZ_RESOLUTION=319\n" +\
+                         "!HORIZ_RESOLUTION=159\n" +\
                          "!VERT_RESOLUTION=91\n" +\
                          "!GRID_TYPE=l_2\n" +\
                          "!UPLOAD_INTERVAL=288.0\n" +\
-                         "!TRICKLE_UPLOAD_FREQUENCY=1\n" +\
                          "!TSTEP=3600\n" +\
                          "&NAMCT0\n"+\
                          " UTSTEP=3600.0,\n" +\
@@ -98,12 +96,18 @@ if __name__ == "__main__":
       zip_file.write('fort.4')
       zip_file.close()
 
-      # Create namelist file
+      # The OpenIFS BOINC implementation uses mapped logical filenames to
+      # identify the various input files.  Here we create the files with
+      # the logical names expected by the model.
+      # It is less than ideal as it makes the code more complex to handle
+      # resolving these files. It also makes it harder to debug as the
+      # filenames are not descriptive.
+
+      # Create logical namelist file
       namelist_string = ">jf_namelist<\n"
       namelist_file=open("oifs_43r3_NNNN_yyyymmddhh_1_d000_0.zip","w")
       namelist_file.write(namelist_string)
       namelist_file.close()
-
 
       # Create ic_ancil file
       ic_ancil_string = ">jf_ic_ancil<\n"
@@ -119,7 +123,6 @@ if __name__ == "__main__":
       zip_file.close()
       shutil.move('./jf_ic_ancil.zip','./jf_ic_ancil')
 
-
       # Create ifsdata file
       ifsdata_string = ">jf_ifsdata<\n"
       ifsdata_file=open("ifsdata_0.zip","w")
@@ -133,7 +136,6 @@ if __name__ == "__main__":
       zip_file.write('jf_ifsdata')
       zip_file.close()
       shutil.move('./jf_ifsdata.zip','./jf_ifsdata')
-
 
       # Create clim_data file
       clim_data_string = ">jf_clim_data<\n"
