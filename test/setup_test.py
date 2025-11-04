6bf46d40b370c12139331081c3ae24495d090c47
Glenn Carver <gdcarver@gmail.com>
2025-11-04 22:03:01 +0000

    make oifs test closer to real OIFS model

--- test/setup_test.py
+++ test/setup_test.py
@@ -19,28 +19,35 @@ if __name__ == "__main__":
     if not os.path.isdir("projects"):
       os.mkdir("projects")
 
-    if not os.path.isdir("slot"):
-      os.mkdir("slot")
+    if not os.path.isdir("slots"):
+      os.mkdir("slots")
+      os.chdir("slots")
+      if not os.path.isdir("0"):
+        os.mkdir("0")
+      os.chdir("../")
 
     os.chdir("projects")
 
-    # Produce fake oifs_43r3_app file
-    with open('oifs_43r3_app_1.00_x86_64-pc-linux-gnu', 'a') as oifs_app_file:
+    # Produce fake oifs_43r3_app file in projects directory
+    oifs_app = "oifs_43r3_app_1.00_x86_64-pc-linux-gnu"
+    with open(oifs_app, 'a') as oifs_app_file:
       oifs_app_file.write(secrets.token_hex(4000) + '\n')
-    zip_file = zipfile.ZipFile('./oifs_43r3_app_1.00_x86_64-pc-linux-gnu.zip','w')
-    zip_file.write('oifs_43r3_app_1.00_x86_64-pc-linux-gnu')
+    zip_file = zipfile.ZipFile(oifs_app+".zip",'w')
+    zip_file.write(oifs_app)
     zip_file.close()
+    os.remove(oifs_app)   # remove unzipped version
 
-    os.chdir("../slot")
+    # Now prepare task files in the slot directory
+    os.chdir("../slots/0")
 
     # Create the init_data.xml, needed by BOINC
     init_data_string = "   <app_init_data>\n" +\
-                       "     <major_version>0</major_version>\n" +\
+                       "     <major_version>1</major_version>\n" +\
                        "     <minor_version>0</minor_version>\n" +\
-                       "     <release>0</release>\n" +\
-                       "     <app_version>000</app_version>\n" +\
+                       "     <release>1</release>\n" +\
+                       "     <app_version>100</app_version>\n" +\
                        "     <hostid>0</hostid>\n" +\
-                       "     <app_name>openifs</app_name>\n" +\
+                       "     <app_name>oifs_43r3</app_name>\n" +\
                        "     <project_preferences></project_preferences>\n" +\
                        "     <project_dir>"+current_path+"/projects</project_dir>\n" +\
                        "     <boinc_dir>"+current_path+"</boinc_dir>\n" +\
@@ -73,21 +80,45 @@ if __name__ == "__main__":
 
 
     # Create the fake model namelist file, fort.4.
+    # TODO: TRICKLE_UPLOAD_FREQUENCY and UPLOAD_NUMBER can be removed as 
+    # they are not used by the controller code any more.
+    # TODO: Should not have TSTEP as well as UTSTEP here, use the namelist variable always!
+
+    # Explanation of variable values in fort.4:
+    # EXPTID = NNNN : Dummy experiment ID
+    # UNIQUE_MEMBER_ID = 1353 : Dummy workunit id
+    # HORIZ_RESOLUTION = 159 : Horizontal resolution l159
+    # VERT_RESOLUTION = 91 : Vertical resolution 91 levels
+    # GRID_TYPE = l_2 : Reduced Gaussian grid type l_2
+    # UPLOAD_INTERVAL = 2880 : Upload interval in seconds (48 model time steps of 1 hour)
+    # UTSTEP = 3600.0 : Model time step in seconds (1 hour)
+    # CUSTOP = 240 : Total number of model time steps to run (240 time steps = 10 days)
+    # CNMEXP = 'NNNN' : Dummy experiment ID
+    # NFRRES = 24 : Frequency of restart file output in model time steps (every 24 time steps = daily restarts)
+    # NFRPOS = 6 : Frequency of post-processed output in model time steps (every 6 time steps = every 6 hrs)
+
     fort_file_string = "!WU_TEMPLATE_VERSION=43r3-seasonal-20250801\n"+\
                          "!EXPTID=NNNN\n"+\
                          "!UNIQUE_MEMBER_ID=1353\n"+\
-                         "!IFSDATA_FILE=ifsdata_0\n" +\
                          "!IC_ANCIL_FILE=ic_ancil_0\n" +\
+                         "!IFSDATA_FILE=ifsdata_0\n" +\
                          "!CLIMATE_DATA_FILE=clim_data_0\n" +\
                          "!HORIZ_RESOLUTION=159\n" +\
                          "!VERT_RESOLUTION=91\n" +\
                          "!GRID_TYPE=l_2\n" +\
-                         "!UPLOAD_INTERVAL=288.0\n" +\
-                         "!TSTEP=3600\n" +\
-                         "&NAMCT0\n"+\
+                         "!UPLOAD_INTERVAL=2880\n" +\
+                         "\n\n"+\
+                         "&NAMARG\n"+\
                          " UTSTEP=3600.0,\n" +\
-                         " NFRPOS=1,\n" +\
-                         " NFRRES=1,\n"
+                         " CUSTOP=240,\n" +\
+                         " CNMEXP='NNNN',\n" +\
+                         "/\n"+\
+                         "&NAMRES\n"+\
+                         " NFRRES=24,\n"+\
+                         "/\n"+\
+                         "&NAMCT0\n"+\
+                         " NFRPOS=6,\n" +\
+                         "/\n"
 
     fort_file=open("fort.4","w")
     fort_file.write(fort_file_string)
@@ -95,6 +126,7 @@ if __name__ == "__main__":
     zip_file = zipfile.ZipFile('./jf_namelist','w')
     zip_file.write('fort.4')
     zip_file.close()
+    os.remove('fort.4')   # remove unzipped version
 
     # The OpenIFS BOINC implementation uses mapped logical filenames to
     # identify the various input files.  Here we create the files with
